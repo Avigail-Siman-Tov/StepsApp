3d7d25543073251549f2e9ca1fec722ab94f4d7e
Avigail-Siman-Tov <avigailsi123@gmail.com>
2023-04-24 12:59:02 +0300

    Initial commit

---
+++ app/src/main/java/com/avigail/stepsapp/SettingActivity.java
@@ -0,0 +1,19 @@
+package com.avigail.stepsapp;
+
+
+import android.app.Activity;
+import android.os.Bundle;
+
+
+public class SettingActivity extends Activity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_setting);
+
+
+    }
+
+}
---
+++ app/src/main/java/com/avigail/stepsapp/SplashActivity.java
@@ -0,0 +1,29 @@
+package com.avigail.stepsapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.widget.ImageView;
+
+public class SplashActivity extends AppCompatActivity {
+
+    private static final int SPLASH_TIME_OUT = 2000; // 2 seconds
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+        new Handler().postDelayed(new Runnable() {
+
+            @Override
+            public void run() {
+                Intent intent = new Intent(SplashActivity.this, SettingActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        }, SPLASH_TIME_OUT);
+    }
+}
